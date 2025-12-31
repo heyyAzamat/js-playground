9d651b0b9b34d2691af331e9bf51b97c63fc5bd0
azaamat.se@gmail.com <azaamat.se@gmail.com>
2025-12-31 15:34:23 +0500

    Changed the name

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -23,7 +23,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class jsplayground {
+public class GuessTheNumber {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
